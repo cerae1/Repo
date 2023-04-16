d73fb65aad7cc9c97a14ba082e5326e04d21338b
batiquin <126436303+batiquin@users.noreply.github.com>
2023-04-16 21:07:11 +0800

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
 import Login from "./Pages/Login";
+import Registration from "./Pages/Registration";
+import Menu from "./Pages/Menu";
+import Address from "./Pages/Address";
+import Address2 from "./Pages/Address2";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
-
-const App = () => {
+function App() {
   return (
-    <div>
-      <Login/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/registration' element={<Registration />} />
+        <Route path='/menu' element={<Menu/>} />
+        <Route path='/address' element={<Address/>}/>
+        <Route path='/address2' element={<Address2/>}/>
+         
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
